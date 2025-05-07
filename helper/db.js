40b1a926181f61e9fff78f52018225dea0a09bd3
imsandeep1073 <s.sandeepmycindia@gmail.com>
2025-05-08 01:10:09 +0530

    create user model

--- helper/db.js
+++ helper/db.js
@@ -1,12 +1,26 @@
 import mongoose from "mongoose"
+import {User} from "../models/user"
 
 export const connectDb = async () => {
     try {
-       const {connection} = await mongoose.connect(process.env.MONGO_DB_URI,{
-            dbName:"work_manger"
+        const { connection } = await mongoose.connect(process.env.MONGO_DB_URI, {
+            dbName: "work_manger"
         })
         console.log("db connected...")
         console.log(connection);
+
+        // testing and create new user
+
+       const uuser =  new User({
+            name:"testing user",
+            email:"testing@gmail.com",
+            password:"12345",
+            about:"this is testing"
+        })
+
+        await uuser.save();
+        console.log('user is created')
+
     }
     catch (error) {
         console.log('Failed to connect..');
---
+++ models/user.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const UserSachema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        required: [true, "Email Required !!"],
+    },
+    password: {
+        type: String,
+        required: [true,"Password Required !!"],        
+    },
+    about:String,
+    profileURL:String,
+    address:{
+        street:String,
+        city:String,
+        country:String,
+        pinCode:Number,
+    }
+
+})
+
+export const User =mongoose.models.users || mongoose.model("users", UserSachema);
